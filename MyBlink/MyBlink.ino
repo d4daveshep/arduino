9cb439609c8b72bffba38a01454eadce408a9fe5
David Shepherd <david@daveshep.net.nz>
2017-12-16 07:38:24 +0000

    moving files to trunk
    git-svn-id: file:///var/svn-repos/arduino/trunk@53 38942e76-d1d7-4647-b734-081872a69eaa

---
+++ MyBlink/MyBlink.ino
@@ -0,0 +1,25 @@
+/*
+  Blink
+ Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+ This example code is in the public domain.
+ */
+
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+const int LED = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(LED, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+}
+
